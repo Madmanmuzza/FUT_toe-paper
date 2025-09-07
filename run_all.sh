8be40df6f3b57ddbf7c627719c7799f5a9e18971
Madmanmuzza <danber1515@gmail.com>
2025-09-08 08:33:06 +1000

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -u bench/calibration.py
-python -u src/experiments/phase_scan.py
-python -u figs/make_phase_diagram.py
-python -u figs/make_dimension_plateaus.py
-python -u figs/make_operator_accuracy.py
-python -u figs/make_interval_distance.py
